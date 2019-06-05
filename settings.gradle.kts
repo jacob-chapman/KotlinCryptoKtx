514eb70ca4612ce50ae0a15bdcfc346351c77450
Jacob Chapman <jacob.chapman@foxsports.com>
2019-06-05 16:33:17 -0400

    removed legacy news module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-include (Modules.app, Modules.nomics, Modules.domain, Modules.news, Modules.newsFeature, Modules.buildSrc, Modules.common_core, Modules.common_network, Modules.common_android)
+include (Modules.app, Modules.nomics, Modules.domain, Modules.newsFeature, Modules.buildSrc, Modules.common_core, Modules.common_network, Modules.common_android)
 project(Modules.common_network).projectDir = File("common/network")
 project(Modules.common_core).projectDir = File("common/core")
 project(Modules.common_android).projectDir = File("common/android")
